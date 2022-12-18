7357dfef8b96572d475ac11ce50e6a25fc134ff2
Irsyad Purbha <irsyadpurbha@gmail.com>
2022-12-18 08:34:38 +0700

    UPDATE 20221218 08:00

--- Pertemuan 13/Contoh 4/contoh4.cpp
+++ Pertemuan 13/Contoh 4/contoh4.cpp
@@ -1,5 +1,6 @@
-#include <stdio.h>
-#include "iostream.h"
+//Contoh Class
+#include <iostream.h>
+#include "stdio.h"
 #include <conio.h>
 
 class motor {
@@ -10,26 +11,9 @@ class motor {
    public:
       //Konstruktor
    	motor(char nama[20], float cc, long hrg);
-      //Destruktor
-      //https://www.tutorialspoint.com/cplusplus/cpp_constructor_destructor.htm
-      ~motor();
       //Fungsi
       void keterangan();
 };
-
-/*
-The Class Destructor
-A destructor is a special member function of a class that is executed whenever
- an object of it's class goes out of scope or whenever the delete expression
- is applied to a pointer to the object of that class.
-
-A destructor will have exact same name as the class prefixed with a tilde (~)
- and it can neither return a value nor can it take any parameters. Destructor
- can be very useful for releasing resources before coming out of the program like closing files, releasing memories etc.
-*/
-motor::~motor() {
-   delete[] merk;
-}
 
 motor::motor(char nama[20], float cc, long hrg) {
 	strcpy(merk,nama);
@@ -49,13 +33,11 @@ void motor::keterangan() {
 	cout<<"Harga motor adalah : "<<harga<<endl;
 }
 
-
-void main()
-{
-	motor balap("Ninja RR",150,35000000);
+void main() {
+	motor balap("Ninja RR",150,40000000);
    balap.keterangan();
    cout<<endl<<endl;
-	motor matik("Mio Soul",110,12500000);
+	motor matik("Mio Soul",110,17500000);
    matik.keterangan();
 	getch();
 }
